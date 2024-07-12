fab9cead8241595d96a918f2261c3b9c73008ed1
NNNNolan <59153876+NNNNolan@users.noreply.github.com>
2024-07-12 10:52:24 +0800

    修改

--- src/HW/Program.cs
+++ src/HW/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//远程
+//远程修改修改
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 System.Console.ReadKey(); 
